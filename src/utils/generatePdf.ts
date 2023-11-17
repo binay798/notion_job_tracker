b743db00065fb6f9f701dd9cb02ab360c30aee38
binay798 <binayshrestha798@gmail.com>
2023-11-17 14:58:41 +0545

    fix: minor fixes with packages

--- src/utils/generatePdf.ts
+++ src/utils/generatePdf.ts
@@ -1,25 +1,25 @@
-import { Request, Response } from 'express';
-import { catchAsync } from './catchAsync';
-import { sendFailureRes, sendSuccessRes } from './formatResponse';
-import HttpStatus from 'http-status-codes';
-import { convertHtmlToPdf } from './convertHtmlToPdf';
-import path from 'path';
-import { PUBLIC_PATH } from '../config/global.constants';
-import mkdirp from 'mkdirp';
+// import { Request, Response } from 'express';
+// import { catchAsync } from './catchAsync';
+// import { sendFailureRes, sendSuccessRes } from './formatResponse';
+// import HttpStatus from 'http-status-codes';
+// import { convertHtmlToPdf } from './convertHtmlToPdf';
+// import path from 'path';
+// import { PUBLIC_PATH } from '../config/global.constants';
+// import mkdirp from 'mkdirp';
 
-export const purContDownloadPurchaseOrderPdf = catchAsync(async (req: Request, res: Response) => {
-  const { html } = req.body;
-  if (!process.env.PURCHASE_ORDER_PDF_PATH) {
-    return sendFailureRes(HttpStatus.BAD_REQUEST)(res, 'Invalid pdf path.')({});
-  }
-  const filePath = path.join(PUBLIC_PATH, process.env.PURCHASE_ORDER_PDF_PATH);
-  const fileName = `${Date.now()}.pdf`;
-  await mkdirp(filePath);
-  const fullPath = path.join(filePath, fileName);
-  await convertHtmlToPdf(encodeURIComponent(html), fullPath);
-  const downloadLink = path.join('/assets/purchase-order', fileName);
+// export const purContDownloadPurchaseOrderPdf = catchAsync(async (req: Request, res: Response) => {
+//   const { html } = req.body;
+//   if (!process.env.PURCHASE_ORDER_PDF_PATH) {
+//     return sendFailureRes(HttpStatus.BAD_REQUEST)(res, 'Invalid pdf path.')({});
+//   }
+//   const filePath = path.join(PUBLIC_PATH, process.env.PURCHASE_ORDER_PDF_PATH);
+//   const fileName = `${Date.now()}.pdf`;
+//   await mkdirp(filePath);
+//   const fullPath = path.join(filePath, fileName);
+//   await convertHtmlToPdf(encodeURIComponent(html), fullPath);
+//   const downloadLink = path.join('/assets/purchase-order', fileName);
 
-  return sendSuccessRes(HttpStatus.OK)(res, 'Successfully to download link')({
-    downloadLink,
-  });
-});
+//   return sendSuccessRes(HttpStatus.OK)(res, 'Successfully to download link')({
+//     downloadLink,
+//   });
+// });
--- src/utils/zipFileAndFolder.ts
+++ src/utils/zipFileAndFolder.ts
@@ -1,40 +1,40 @@
-import fs from 'fs';
-import archiver from 'archiver';
+// import fs from 'fs';
+// import archiver from 'archiver';
 
-export const generateZipFile = async (docToBeZippedLoc: string, zipFilePath: string, fileNameOnZip = 'file.txt') => {
-  const docStats = await fs.statSync(docToBeZippedLoc);
+// export const generateZipFile = async (docToBeZippedLoc: string, zipFilePath: string, fileNameOnZip = 'file.txt') => {
+//   const docStats = await fs.statSync(docToBeZippedLoc);
 
-  return new Promise((resolve, reject) => {
-    // Create a writable stream for the zip file
-    const output = fs.createWriteStream(zipFilePath);
+//   return new Promise((resolve, reject) => {
+//     // Create a writable stream for the zip file
+//     const output = fs.createWriteStream(zipFilePath);
 
-    // Create a new archive instance
-    const archive = archiver('zip', {
-      zlib: { level: 9 }, // Set compression level (0-9)
-    });
+//     // Create a new archive instance
+//     const archive = archiver('zip', {
+//       zlib: { level: 9 }, // Set compression level (0-9)
+//     });
 
-    // Pipe the archive data to the output stream
-    archive.pipe(output);
+//     // Pipe the archive data to the output stream
+//     archive.pipe(output);
 
-    if (docStats.isDirectory()) {
-      // Append files from a sub-directory, putting its contents at the root of archive
-      archive.directory(docToBeZippedLoc, false);
-    } else {
-      // Append file to zip file on root
-      archive.file(docToBeZippedLoc, { name: fileNameOnZip });
-    }
+//     if (docStats.isDirectory()) {
+//       // Append files from a sub-directory, putting its contents at the root of archive
+//       archive.directory(docToBeZippedLoc, false);
+//     } else {
+//       // Append file to zip file on root
+//       archive.file(docToBeZippedLoc, { name: fileNameOnZip });
+//     }
 
-    // Finalize the archive (this will create the zip file)
-    archive.finalize();
+//     // Finalize the archive (this will create the zip file)
+//     archive.finalize();
 
-    // Listen for all archive data to be written
-    output.on('close', () => {
-      return resolve(zipFilePath);
-    });
+//     // Listen for all archive data to be written
+//     output.on('close', () => {
+//       return resolve(zipFilePath);
+//     });
 
-    // Listen for errors during the archiving process
-    archive.on('error', (err) => {
-      return reject(err);
-    });
-  });
-};
+//     // Listen for errors during the archiving process
+//     archive.on('error', (err) => {
+//       return reject(err);
+//     });
+//   });
+// };
