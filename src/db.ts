8b77a5eeedf6883ac2236f713db730a094720f0e
binay798 <binayshrestha798@gmail.com>
2023-11-26 12:57:06 +0545

    fix: minor fixes

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,13 @@
 import knex from 'knex';
 import * as knexConfig from './knexfile';
+import Boom from '@hapi/boom';
 
 const _knex = knex(knexConfig);
 
-const x = _knex.transaction();
+const x = _knex.transaction().catch(() => {
+  throw Boom.badData('Database connection issue.');
+});
+x.then((res) => res.commit());
 type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
 export type Transaction = UnwrapPromise<typeof x>;
 
